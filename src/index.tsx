564fd53484df016a49a1e3997199e096d1e0b48f
tmoka0476 <knowledzr+alt@gmail.com>
2021-02-24 06:44:18 +0900

    set up prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react"
+import ReactDom from "react-dom"
 
 const App = () => {
-    return (
-        <h1>Hello React!</h1>
-    )
+  return <h1>Hello React!</h1>
 }
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(<App />, document.getElementById("app"))
