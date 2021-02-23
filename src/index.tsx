545d86ff660b9d5b8b01ca46cab4df9ff3224c04
tmoka0476 <knowledzr+alt@gmail.com>
2021-02-24 06:35:28 +0900

    set up React

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+const App = () => {
+    return (
+        <h1>Hello React!</h1>
+    )
+}
+
+ReactDom.render(<App />, document.getElementById("app"));
